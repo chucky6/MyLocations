76fcaa558e45962056ced2a71e0e544d5d186d27
chucky <antonioalvesdev@gmail.com>
2016-02-03 22:38:15 -0200

    layout finished

---
+++ MyLocations/MyNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  MyNavigationController.swift
+//  MyLocations
+//
+//  Created by Antonio Alves on 2/3/16.
+//  Copyright © 2016 Antonio Alves. All rights reserved.
+//
+
+import UIKit
+
+class MyNavigationController: UINavigationController {
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+}
